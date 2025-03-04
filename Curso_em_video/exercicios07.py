85b745db6c8c47d0146cf3cf68e4fd2bae4bed42
Lucas <lucaslopesgomes06@gmail.com>
2025-03-04 01:21:47 -0300

    feito exercicio 07

---
+++ Curso_em_video/exercicios07.py
@@ -0,0 +1,6 @@
+nota1 = float(input("digite a nota da primeira prova: \n").replace(",", "."))
+nota2 = float(input("digite a nota da segunda prova: \n").replace(",", "."))
+
+media = (nota1 + nota2) / 2
+
+print(f"A media é {media}")
