1d3f9b6c494cf8cc477881bf9784fb7867040ef8
Lucas <lucaslopesgomes06@gmail.com>
2025-03-04 19:40:46 -0300

    exercicios15 concluido

---
+++ Curso_em_video/exercicios15.py
@@ -0,0 +1,15 @@
+def linha_inicio_fim():
+    print("_" * 50)
+
+
+dias_alugados = int(input("Por quantos dias o carro ficou alugado? \n"))
+km_rodados = float(input("Quantos Km foram rodados? \n"))
+
+pg_dias = dias_alugados * 60
+pg_km = km_rodados * 0.15
+
+linha_inicio_fim()
+print(
+    f"O total a ser pagos é R${pg_dias + pg_km:.2f} \n R${pg_dias:.2f}referente aos dias alugados \n R${pg_km}referente aos quilometros rodados"
+)
+linha_inicio_fim()
