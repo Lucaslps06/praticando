126a9cc495a967f8598c7d0875eea4ce1fa33127
Lucas <lucaslopesgomes06@gmail.com>
2025-03-03 22:30:24 -0300

    exercicios_CV

---
+++ Curso_em_video/exercicios03.py
@@ -0,0 +1,4 @@
+number1 = int(input("Digite um numero \n"))
+number2 = int(input("Digite outro \n"))
+resultado = number1 + number2
+print("A soma de {} e {} é igual a {}".format(number1, number2, resultado))
---
+++ Curso_em_video/exercicios04.py
@@ -0,0 +1,11 @@
+D = input("digite algo \n")
+
+print(type(D))
+
+print("É alfanumerico?", D.isalnum())
+print("É numérico? ", D.isnumeric())
+print("É letra?", D.isalpha())
+print("É decimal?", D.isdecimal())
+print("É um espaço?", D.isspace())
+print("É maiúsculo?", D.istitle())
+print("É minusculo?", D.islower())
