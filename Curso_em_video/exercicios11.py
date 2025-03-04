ad4304e41f231c5b9ad4dcefa7d7701fb50c1daa
Lucas <lucaslopesgomes06@gmail.com>
2025-03-04 11:47:40 -0300

    exercicios11 concluido

---
+++ Curso_em_video/exercicios11.py
@@ -0,0 +1,9 @@
+altura = float(input("Me fale a altura da parede \n").replace(",", "."))
+comprimento = float(input("e o comprimento \n").replace(",", "."))
+
+# altura.isnumeric or altura.is
+area = altura * comprimento
+
+tinta = area // 2
+
+print(f"A area {area} prescisa de {tinta} litros de tinta para pintar")
