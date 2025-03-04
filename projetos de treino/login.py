a8b0f313207ea4bc77c018e2dbf6740f60eaf85b
Lucas <lucaslopesgomes06@gmail.com>
2025-03-04 02:31:31 -0300

    create login

---
+++ projetos de treino/login.py
@@ -0,0 +1,8 @@
+email = input("Digite seu email \n")
+senha = input("Digite sua senha \n")
+
+if email == "teste@gmail" and senha == "senha123":
+    print("Acesso permitido, seja bem vindo!")
+
+else:
+    print("Acesso negado")
