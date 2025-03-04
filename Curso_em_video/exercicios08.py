c628888a915d8297e1e6920a9711a33ae839a19f
Lucas <lucaslopesgomes06@gmail.com>
2025-03-04 01:32:58 -0300

    exercicios 8 feito

---
+++ Curso_em_video/exercicios08.py
@@ -0,0 +1,7 @@
+metros = input("Digite um valor em metros: \n")
+
+mm = float(metros) * 1000
+cm = float(metros) * 100
+
+print(f"{metros} metros é igual a {cm} centímetros")
+print(f"{metros} metros é igual a {mm} milímetros")
